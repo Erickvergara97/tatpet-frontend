6b64f4d6389b1949552f9b0ab222f939c0d948ac
Erick Vergara <erick.vergara.ruiz@gmail.com>
2025-03-28 21:43:18 -0500

    flowbite config

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from 'next/font/google';
 import '../styles/globals.css';
 import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
+import { ThemeModeScript } from 'flowbite-react';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -26,6 +27,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <ThemeModeScript />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
